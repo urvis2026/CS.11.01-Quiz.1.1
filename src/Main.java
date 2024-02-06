28e8099dc544fa3018bf861fad65e372466ec944
urvisavant <urvis2026@student.cis.edu.hk>
2024-02-06 11:29:59 +0800

    CS.11.01-Quiz.1.1

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,17 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
-
-
-
+    Scanner.scanner = new Scanner (System.in);
+    int age;
+    String firstName;
+    String favoriteFood;
+    System.out.println ("enter your age:");
+    age = scanner.nextInt();
+    int age = scanner.nextInt();
+    system.out.println ("enter your first name:");
+    firstName = scanner.nextChar();
+    system.out.println ("what's your favorite food?");
+    favoriteFood = scanner;
     }
 
 }
